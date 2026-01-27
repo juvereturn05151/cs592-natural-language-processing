57c334ac6b3b3af5a9ded6c0f2aed34695bfbe6c
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-01-27 00:00:04 -0800

    Update InClassAssignment for NLP Project

--- Chankasemporn_Ju-ve_CS592_NLP_Project/src/TokenizerHelper.py
+++ Chankasemporn_Ju-ve_CS592_NLP_Project/src/TokenizerHelper.py
@@ -5,6 +5,15 @@ Copyright:    (c) 2025 DigiPen Institute of Technology. All rights reserved.
 """
 
 import nltk
+import re
+import string
+from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus import stopwords
+
+# Initialize tokenizer and stopword list
+wp = WordPunctTokenizer()
+stopWordList = set(stopwords.words("english"))
+
 
 def getTokensSplit(item):
     textBlock = item.text
@@ -12,14 +21,36 @@ def getTokensSplit(item):
     tokenlist = textBlock.split()
     return tokenlist
 
+
+def getTokens_NLTK_Tokenize(item):
+    textBlock = item.text.lower()
+    # 2. Using nltk.tokenize.word_tokenize
+    tokenlist = nltk.tokenize.word_tokenize(textBlock)
+    return tokenlist
+
+
+def getTokens_NLTK_PunktTokenize(item):
+    textBlock = item.text.lower()
+    # 3. Using nltk.WordPunctTokenizer.tokenize
+    tokenlist = [
+        re.sub(r"[\'—_“”\"\,\?.’‘\-\)\:!\&]", "", word)
+        for word in wp.tokenize(textBlock)
+        if word not in stopWordList
+        and word not in string.punctuation
+        and word.strip() != ""
+    ]
+    return tokenlist
+
+
 def buildWordMap(documentTokens):
     wordMap = dict()
     for word in documentTokens:
-        if word in wordMap :
+        if word in wordMap:
             wordMap[word] += 1
         else:
             wordMap[word] = 1
     return wordMap
+
 
 # This is a hack to print the words from the dictionary into a sorted order
 def printSortedMap(inputMap):
@@ -27,11 +58,5 @@ def printSortedMap(inputMap):
     for key, value in inputMap.items():
         dummyList.append([value, key])
     dummyList = sorted(dummyList, reverse=True)
-    print( dummyList )
+    print(dummyList)
     return dummyList
-
-def getTokens_NLTK_Tokenize(item):
-    textBlock = item.text.lower()
-        # 2. Now using nltk.tokenize to get keywords
-    tokenlist = nltk.tokenize.word_tokenize(textBlock)
-    return tokenlist
