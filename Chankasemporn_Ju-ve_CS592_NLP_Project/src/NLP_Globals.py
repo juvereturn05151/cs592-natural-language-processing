8d479a449dec1d4dba8f5f4f90f869345e7689f5
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-01-29 15:22:19 -0800

    Add extracting keywords

---
+++ Chankasemporn_Ju-ve_CS592_NLP_Project/src/NLP_Globals.py
@@ -0,0 +1,8 @@
+"""
+File Name:    NLP_Globals.py
+Author(s):    Ju-ve Chankasemporn
+Copyright:    (c) 2025 DigiPen Institute of Technology. All rights reserved.
+"""
+
+trainDir = "./data/train/"
+testDir = "./data/test/"
--- Chankasemporn_Ju-ve_CS592_NLP_Project/src/TokenizerHelper.py
+++ Chankasemporn_Ju-ve_CS592_NLP_Project/src/TokenizerHelper.py
@@ -15,48 +15,53 @@ wp = WordPunctTokenizer()
 stopWordList = set(stopwords.words("english"))
 
 
-def getTokensSplit(item):
-    textBlock = item.text
-    # 1. Using the built-in tokenizer from Python string
-    tokenlist = textBlock.split()
-    return tokenlist
+class TokenizerHelper:
+
+    def __init__(self):
+        pass
+
+    def getTokensSplit(self, item):
+        textBlock = item.text
+        # 1. Using the built-in tokenizer from Python string
+        tokenlist = textBlock.split()
+        return tokenlist
 
 
-def getTokens_NLTK_Tokenize(item):
-    textBlock = item.text.lower()
-    # 2. Using nltk.tokenize.word_tokenize
-    tokenlist = nltk.tokenize.word_tokenize(textBlock)
-    return tokenlist
+    def getTokens_NLTK_Tokenize(self, item):
+        textBlock = item.text.lower()
+        # 2. Using nltk.tokenize.word_tokenize
+        tokenlist = nltk.tokenize.word_tokenize(textBlock)
+        return tokenlist
 
 
-def getTokens_NLTK_PunktTokenize(item):
-    textBlock = item.text.lower()
-    # 3. Using nltk.WordPunctTokenizer.tokenize
-    tokenlist = [
-        re.sub(r"[\'—_“”\"\,\?.’‘\-\)\:!\&]", "", word)
-        for word in wp.tokenize(textBlock)
-        if word not in stopWordList
-        and word not in string.punctuation
-        and word.strip() != ""
-    ]
-    return tokenlist
+    def getTokens_NLTK_PunktTokenize(self, item):
+        textBlock = item.text.lower()
+        # 3. Using nltk.WordPunctTokenizer.tokenize
+        tokenlist = [
+            re.sub(r"[\'—_“”\"\,\?.’‘\-\)\:!\&]", "", word)
+            for word in wp.tokenize(textBlock)
+            if word not in stopWordList
+            and word not in string.punctuation
+            and word.strip() != ""
+        ]
+        return tokenlist
 
 
-def buildWordMap(documentTokens):
-    wordMap = dict()
-    for word in documentTokens:
-        if word in wordMap:
-            wordMap[word] += 1
-        else:
-            wordMap[word] = 1
-    return wordMap
+    def buildWordMap(self, documentTokens):
+        wordMap = dict()
+        for word in documentTokens:
+            if word in wordMap:
+                wordMap[word] += 1
+            else:
+                wordMap[word] = 1
+        return wordMap
 
 
-# This is a hack to print the words from the dictionary into a sorted order
-def printSortedMap(inputMap):
-    dummyList = []
-    for key, value in inputMap.items():
-        dummyList.append([value, key])
-    dummyList = sorted(dummyList, reverse=True)
-    print(dummyList)
-    return dummyList
+    # This is a hack to print the words from the dictionary into a sorted order
+    def printSortedMap(self, inputMap):
+        dummyList = []
+        for key, value in inputMap.items():
+            dummyList.append([value, key])
+        dummyList = sorted(dummyList, reverse=True)
+        print(dummyList)
+        return dummyList
