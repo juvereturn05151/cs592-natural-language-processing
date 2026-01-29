67d454ff18a1efe4eca1b0452a61b098348d833b
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-01-29 01:24:55 -0800

    add main.py

---
+++ Chankasemporn_Ju-ve_CS592_NLP_Project/src/main.py
@@ -0,0 +1,11 @@
+"""
+File Name:    main.py.py
+Author(s):    Ju-ve Chankasemporn
+Copyright:    (c) 2025 DigiPen Institute of Technology. All rights reserved.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
