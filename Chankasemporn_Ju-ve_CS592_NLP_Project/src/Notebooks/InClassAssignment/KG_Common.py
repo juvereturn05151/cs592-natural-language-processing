89ba6682a02a7dbd555a6e20aeee766b9379fb46
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-03-18 10:28:03 -0700

    update data storage and KG_Commons

--- Chankasemporn_Ju-ve_CS592_NLP_Project/src/Notebooks/InClassAssignment/KG_Common.py
+++ Chankasemporn_Ju-ve_CS592_NLP_Project/src/Notebooks/InClassAssignment/KG_Common.py
@@ -3,6 +3,55 @@ File Name:    KG_Common.py
 Author(s):    Ju-ve Chankasemporn
 Copyright:    (c) 2025 DigiPen Institute of Technology. All rights reserved.
 """
+
+import spacy
+from spacy.tokens import Span
+
+def merge_tokens_to_ner(doc, token_list, label="PERSON"):
+    """
+    Merge consecutive tokens into a single named entity span.
+
+    Parameters
+    ----------
+    doc : spacy.tokens.Doc
+    token_list : list[str]
+        Tokens to merge (e.g., ["DON", "PEDRO"])
+    label : str
+        NER label to assign (default: PERSON)
+
+    Returns
+    -------
+    doc : spacy.tokens.Doc
+    """
+
+    phrase = " ".join(token_list)
+    phrase_len = len(token_list)
+
+    new_ents = list(doc.ents)
+
+    i = 0
+    while i < len(doc):
+        # Check if tokens match the phrase
+        match = True
+        for j in range(phrase_len):
+            if i + j >= len(doc) or doc[i + j].text.upper() != token_list[j]:
+                match = False
+                break
+
+        if match:
+            span = Span(doc, i, i + phrase_len, label=label)
+
+            # Remove overlapping entities
+            new_ents = [ent for ent in new_ents if not (ent.start < span.end and span.start < ent.end)]
+
+            new_ents.append(span)
+            i += phrase_len
+        else:
+            i += 1
+
+    doc.ents = new_ents
+    return doc
+
 def displayEntities(doc):
     """
     Display named entities in a spaCy Doc.
